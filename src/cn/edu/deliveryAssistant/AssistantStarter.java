287460cdd08c3b81136c241de0d2113d8ad505c5
onshero <1064610021@qq.com>
2020-07-03 11:43:29 +0800

    third

--- src/cn/edu/deliveryAssistant/AssistantStarter.java
+++ src/cn/edu/deliveryAssistant/AssistantStarter.java
@@ -1,10 +1,15 @@
 package cn.edu.deliveryAssistant;
 
-public class AssistantStarter {
+import cn.edu.zucc.personplan.ui.FrmMain;
 
+public class AssistantStarter {
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new FrmMain();
 	}
+
 
 }
